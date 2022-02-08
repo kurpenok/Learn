5f8702b9f12aa5de160fe0dd2bd2f15e51b470c6
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-08 10:44:08 +0600

    Finished task by create structures on Golang

--- Golang/main.go
+++ Golang/main.go
@@ -3,8 +3,30 @@ package main
 import ("fmt"
 		"net/http")
 
+type User struct {
+	name string
+	age uint16
+	money int16
+	avg_grades, happiness float64
+}
+
+func (u *User) getAllInfo() string {
+	return fmt.Sprintf("" +
+		"[+] User name: %s\n" +
+		"[+] Age: %d\n" +
+		"[+] Money: %d\n",
+		u.name, u.age, u.money)
+}
+
+func (u *User) setNewName(name string) {
+	u.name = name
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go was started!")
+	bob := User{"Bob", 21, 10243, 4.2, 0.8}
+	bob.setNewName("Alex")
+	fmt.Fprintf(w, bob.getAllInfo())
+
 }
 
 func contacts(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +41,9 @@ func handle() {
 }
 
 func main() {
+	// var bob User = ...
+	// bob := User{name: "Bob", age: 21, money: 10243, avg_grades: 4.2, happiness: 0.8}
+
 	handle()
 }
 
