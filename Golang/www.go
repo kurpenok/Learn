19a442c0f0506d11aeae0cab8390b8d56ade6079
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-10 08:40:57 +0600

    Created test website with Golang

---
+++ Golang/www.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func handle() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func main() {
+	handle()
+}
+
