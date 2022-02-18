134748e07d04bd804130441936a26baec03fc661
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-18 22:47:03 +0600

    Added processed properties to Vue

--- Vue/js/main.js
+++ Vue/js/main.js
@@ -3,12 +3,24 @@ new Vue ({
     
     data: {
         text: "Hello, world!",
-        style: ""
+        style: "",
+        value: 1,
     },
 
     methods: {
         change () {
             this.text = "Implemented method was runned"
+        }
+        increment (value) {
+            this.value = value
+            if (value == 25) {
+                alert("Number 25")
+            }
+        }
+    },
+    computed: {
+        new_value () {
+            return this.value * 2
         }
     }
 });
