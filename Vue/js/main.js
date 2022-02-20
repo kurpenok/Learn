a12c209c34515c135c3eec79c6cc2a1a3491050b
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-20 17:49:52 +0600

    Added conditional Vue operators

--- Vue/js/main.js
+++ Vue/js/main.js
@@ -5,12 +5,17 @@ new Vue ({
         text: "Hello, world!",
         style: "",
         value: 1,
+        show: true,
+        cars: [
+            {model: "Lada", speed: 200},
+            {model: "UAZ", speed: 100}
+        ]
     },
 
     methods: {
         change () {
             this.text = "Implemented method was runned"
-        }
+        },
         increment (value) {
             this.value = value
             if (value == 25) {
