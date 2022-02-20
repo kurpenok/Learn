29811a7b2af1099fa7665ee1c8a39d3c9d75916b
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-20 18:43:31 +0600

    Added Vue CLI components

---
+++ Vue/project/src/Example.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Example text</h1>
+    </div>
+</template>
+
+<script>
+import Input from './Field.vue';
+
+export default {
+    components: {
+        "app-input": Input
+    }
+}
+
+</script>
+
