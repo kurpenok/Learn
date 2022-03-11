c3f833964d7492c4fe2147955208a15cc2f568aa
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-03-11 11:00:29 +0600

    Added Flask HTML templates and Bootstrap styles

--- Flask/app.py
+++ Flask/app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return "<h1>Hello, Flask!</h1>"
+    return render_template("index.html")
 
 
 @app.route("/some")
 def some():
-    return "<h1>Some page</h1>"
+    return render_template("some.html")
+
+
+@app.route("/user/<string:name>/<int:identifier>")
+def user(name: str, identifier: int):
+    return f"User page: {name} - {identifier}"
 
 
 if __name__ == "__main__":
