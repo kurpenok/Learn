278391e701a0f37d76b15e077f0af175a0a0b579
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-03-12 17:10:40 +0600

    Added create article page

--- Flask/app.py
+++ Flask/app.py
@@ -1,22 +1,50 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///articles.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    identifier = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return "<Article %r>" % self.identifier
+
 
 @app.route("/")
-@app.route("/home")
 def index():
     return render_template("index.html")
 
 
-@app.route("/some")
-def some():
-    return render_template("some.html")
+@app.route("/create", methods=["POST", "GET"])
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        intro = request.form["intro"]
+        text = request.form["text"]
 
-
-@app.route("/user/<string:name>/<int:identifier>")
-def user(name: str, identifier: int):
-    return f"User page: {name} - {identifier}"
+        article = Article(title=title, intro=intro, text=text)
+        
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "[-] ERROR! Article was not added!"
+    else:
+        return render_template("create.html")
 
 
 if __name__ == "__main__":
